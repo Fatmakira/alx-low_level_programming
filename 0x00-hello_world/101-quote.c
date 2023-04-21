63bad92228abd6dc9a555b47f3fde1a28fc9403b
fatmakira <fatoma_320@hotmail.com>
2023-04-20 19:23:15 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main -Entery point
@@ -7,3 +6,4 @@
 int main(void){
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);
+}
