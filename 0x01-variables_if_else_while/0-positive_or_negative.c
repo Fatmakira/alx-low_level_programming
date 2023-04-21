eafcb0b012b9779f24b58e38447d4e9809eec1df
fatmakira <fatoma_320@hotmail.com>
2023-04-21 16:49:44 -0700

    variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 /**
  * main - entry point
  *
- * description: positive or negative validation
+ * Description: positive or negative validation
  *
- * return: 0 (success)
+ * Return: 0 (success)
 */
 
 int main(void)
