deeb3d7e0259ce27ff0216aa68c5454e8603c236
fatmakira <fatoma_320@hotmail.com>
2023-04-26 04:54:31 -0700

    nested

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result
+ *
+*/
+
+int add(int a, int b)
+{
+        int sum = a + b;
+
+        return (sum);
+        _putchar('\n');
+}
