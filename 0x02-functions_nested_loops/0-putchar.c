490e9a5ed96d9678f70fee15146c1395c06e0306
fatmakira <fatoma_320@hotmail.com>
2023-04-26 01:36:45 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main -entry point
+ *
+ * Description: write a program that prints _putchar
+ *
+ * Return Always (0) success
+ *
+*/
+int main(void)
+{
+	_putchar('_putchar\n');
+	return (0);
+}
